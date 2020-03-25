e21a467fa9132c5ca8e773c0fb2dc7867e2d978a
kennetha123 <jodysag5@gmail.com>
2020-03-26 06:16:57 +0700

    Add Window

--- OpenGL Triangle Shader/OpenGL Triangle Shader/main.cpp
+++ OpenGL Triangle Shader/OpenGL Triangle Shader/main.cpp
@@ -1,8 +1,62 @@
+#include<glad\glad.h>
+#include<GLFW\glfw3.h>
+
 #include <iostream>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void ProcessInput(GLFWwindow* window);
+void framesize_buffer_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Triangle Shaders", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Window is not initialized." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framesize_buffer_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "GLAD is not initialized." << std::endl;
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		ProcessInput(window);
+
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
 	return 0;
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void framesize_buffer_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
