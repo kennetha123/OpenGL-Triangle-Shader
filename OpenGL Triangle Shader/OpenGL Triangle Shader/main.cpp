4b2b50e2724170cff313b293d0002a13f831dec4
kennetha123 <jodysag5@gmail.com>
2020-03-26 06:49:25 +0700

    Add Triangles

--- OpenGL Triangle Shader/OpenGL Triangle Shader/main.cpp
+++ OpenGL Triangle Shader/OpenGL Triangle Shader/main.cpp
@@ -9,6 +9,20 @@ const unsigned int SCR_HEIGHT = 600;
 void ProcessInput(GLFWwindow* window);
 void framesize_buffer_callback(GLFWwindow* window, int width, int height);
 
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout(location = 0)in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 fragColor;\n"
+"void main()\n"
+"{fragColor = vec4(0.7f, 0.5f, 0.2f, 1.0f);}\0";
+
 int main()
 {
 	glfwInit();
@@ -34,12 +48,50 @@ int main()
 		return -1;
 	}
 
+	//Create Shader
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO, VAO;
+
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(VAO);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		ProcessInput(window);
 
 		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
